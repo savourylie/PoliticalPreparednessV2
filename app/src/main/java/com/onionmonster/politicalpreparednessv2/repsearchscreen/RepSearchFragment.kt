6558a59f04ec7cbb264f1ecaf2a5fafdf4f8bad2
Calvin Ku <calvin.j.ku@googlemail.com>
2021-11-05 19:09:39 +0800

    Added some logs.

--- app/src/main/java/com/onionmonster/politicalpreparednessv2/repsearchscreen/RepSearchFragment.kt
+++ app/src/main/java/com/onionmonster/politicalpreparednessv2/repsearchscreen/RepSearchFragment.kt
@@ -1,6 +1,7 @@
 package com.onionmonster.politicalpreparednessv2.repsearchscreen
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -59,8 +60,10 @@ class RepSearchFragment : Fragment() {
         }
 
         viewModel.navigateToRepList.observe(viewLifecycleOwner, {
+            Log.d(TAG, "navigateToRepList: " + it.toString())
+
             if (it != null) {
-                this.findNavController().navigate(
+                findNavController().navigate(
                     RepSearchFragmentDirections.actionRepsearchToResult(it)
                 )
 
